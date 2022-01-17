a73aa2e02f62922b48c0dbeffff479f38607eb97
shiauweitsung <zz830527@gmail.com>
2022-01-17 18:26:28 +0800

    linear-gradient add

--- App.js
+++ App.js
@@ -34,6 +34,11 @@ import PageMain from './components/pages/pageMain';
 import PageTwo from './components/pages/page2';
 import Page3 from './components/pages/page3';
 import LolScreen from './components/pages/Lol';
+//linear gradient
+import LinearGradient from 'react-native-linear-gradient';
+//icon
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 //draw
 
 // import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -96,40 +101,74 @@ function HomeScreen({ navigation, route }) {
     navigation.navigate({ name: 'LolPage' })
   }
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={styles.Button}>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details', {
-          itemID: Math.floor(Math.random() * 100),
-          otherParam: 'anything',
-          txt: 'testtxt'
-        })}
-      />
-      <Button title="screen" onPress={() => screen(navigation)}>
-      </Button>
-      <Button title="abc" onPress={() => abc('123')}>
-      </Button>
-      <View style={{ margin: 20 }}>
-        <Button color="#f194ff" title="go post" onPress={() => navigation.navigate({
-          name: 'Post'
-        })} />
-        <Text style={{ margin: 10 }}>post : {route.params?.post}</Text>
-        <Text onPress={() => abc('txt')}>文字function</Text>
+    <ScrollView>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={styles.Button}>Home Screen</Text>
+        <Icon name={'facebook'} size={24} color={'#999'} />
+        <Button
+          title="Go to Details"
+          onPress={() => navigation.navigate('Details', {
+            itemID: Math.floor(Math.random() * 100),
+            otherParam: 'anything',
+            txt: 'testtxt'
+          })}
+        />
+        <Button title="screen" onPress={() => screen(navigation)}>
+        </Button>
+        <Button title="abc" onPress={() => abc('123')}>
+        </Button>
+        <View style={{ margin: 20 }}>
+          <Button color="#f194ff" title="go post" onPress={() => navigation.navigate({
+            name: 'Post'
+          })} />
+          <Text style={{ margin: 10 }}>post : {route.params?.post}</Text>
+          <Text onPress={() => abc('txt')}>文字function</Text>
+        </View>
+        <View>
+          <Button title="more stack" onPress={() => {
+            navigation.navigate('MorePages')
+          }} />
+        </View>
+        <View style={{ margin: 20 }}>
+          <TouchableOpacity style={styles.lolBtn} onPress={() => goLolScreen(navigation)}>
+            <Text style={styles.lol_txt}>
+              Go LOL
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ marginBottom: 10 }}>
+          <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
+            <Text style={styles.buttonText}>
+              Sign in with Facebook
+            </Text>
+          </LinearGradient>
+        </View>
+        <LinearGradient
+          colors={['#451DFF', '#FF1D7C']}
+          start={{ x: 0.0, y: 1.0 }} end={{ x: 1.0, y: 1.0 }}
+          style={{ height: 48, width: 200, alignItems: 'center', justifyContent: 'center', width: 200 }}
+        >
+          <TouchableOpacity style={styles.linearBtn}>
+            <Text style={styles.linearTxt}>
+              LOGIN
+            </Text>
+          </TouchableOpacity>
+        </LinearGradient>
+        <View style={{ marginTop: 30 }}>
+          <LinearGradient colors={['#19113E', '#041D75']}
+            start={{ x: 0.6, y: 0.4 }} end={{ x: 1.0, y: 1.0 }}
+            style={{
+              height: 600, width: 200, borderBottomRightRadius: 40, alignItems: 'center',
+              justifyContent: 'center'
+            }}>
+            <TouchableOpacity style={{ width: 100, height: 50, alignItems: 'center' }}>
+              <Text style={{ borderColor: 'white', borderWidth: 1, color: 'white', padding: 15, }}>login</Text>
+            </TouchableOpacity>
+          </LinearGradient>
+        </View>
       </View>
-      <View>
-        <Button title="more stack" onPress={() => {
-          navigation.navigate('MorePages')
-        }} />
-      </View>
-      <View style={{ margin: 20 }}>
-        <TouchableOpacity style={styles.lolBtn} onPress={() => goLolScreen(navigation)}>
-          <Text style={styles.lol_txt}>
-            Go LOL
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    </ScrollView>
+
   );
 }
 function DetailsScreen({ route, navigation }) {
@@ -249,7 +288,36 @@ const styles = StyleSheet.create({
   },
   lol_txt: {
     color: 'white'
-  }
+  },
+  linearGradient: {
+    // flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
+  linearBtn: {
+    width: 200,
+    alignItems: 'center',
+    flex: 1,
+  },
+  linearTxt: {
+    textAlign: 'center',
+    color: 'red',
+    padding: 15,
+    marginLeft: 1,
+    marginRight: 1,
+    margin: 1,
+    width: 198,
+    backgroundColor: 'transparent'
+  },
 });
 
 export default App;
